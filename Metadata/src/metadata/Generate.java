4eaba057d552aa738082dce7ab977368868a4d51
aaoeclipse <santiago.paiz@gmail.com>
2017-03-21 18:46:30 -0600

    testing

--- Metadata/src/metadata/Generate.java
+++ Metadata/src/metadata/Generate.java
@@ -2,9 +2,9 @@ package metadata;
 
 public class Generate {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		InstMetadata meta = new Metadata_impl();
 		meta.createFolder("database1");
 		meta.createFile("database1", "table1");
-	}
+	}*/
 }
