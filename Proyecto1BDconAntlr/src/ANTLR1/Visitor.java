50499ce7a774c416fa6ed8e5ebd91aae42c8a915
Andres Oliva <oli12149@uvg.edu.gt>
2017-04-02 18:19:53 -0600

    visitor

--- Proyecto1BDconAntlr/src/ANTLR1/Visitor.java
+++ Proyecto1BDconAntlr/src/ANTLR1/Visitor.java
@@ -1,11 +1,15 @@
 package ANTLR1;
 
+import java.awt.Frame;
+import java.util.ArrayList;
+
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import ANTLR1.DATABASEBaseVisitor;
 import ANTLR1.DATABASEParser.AlterDbStmtContext;
 import ANTLR1.DATABASEParser.AlterNameContext;
 import ANTLR1.DATABASEParser.ChNombreContext;
+import ANTLR1.DATABASEParser.ColConstraintContext;
 import ANTLR1.DATABASEParser.ColNameContext;
 import ANTLR1.DATABASEParser.ColumnContext;
 import ANTLR1.DATABASEParser.ColumnListContext;
@@ -194,8 +198,288 @@ public class Visitor extends DATABASEBaseVisitor<Object> {
 	}
 	
 	@Override
+	public Object visitColConstraint(ColConstraintContext ctx) {
+		//Se debe revisar el type del constraint
+		if(ctx.PRIMARY()!=null){ //Si es Primary key
+            String nombre = ctx.pkNombre().getText();
+            //Revisamos que no exista una primary key en las constraints declaradas antes
+            boolean hay_pk = hasPK(availableCons);
+            if(hay_pk){
+                    Debug.add("Error: No es posible declarar dos primary keys. En la table: "+tableCreate.name);
+                    if(!Frame.activateVerbose){
+                        Frame.jTextArea2.setText("Error: No es posible declarar dos primary keys. En la table: "+tableCreate.name);
+                    }
+                    return "Error";
+            }
+            
+            //Revisando que existan los nombres de las columnas
+            //Nota: se debe cambiar Column a nuestro metadata para columnas.
+            ArrayList<Column> pkCols = new ArrayList<Column>();
+
+            ArrayList<Integer> colIndices = new ArrayList<Integer>();
+            for(ParseTree n:ctx.localids()){
+                String text = n.getText();
+                Column found = getColumn(text,colsCreate);
+
+                if(found==null){
+                    Debug.add("Error: No se encuentra la column: "+text+" En la table: "+tableCreate.name);
+                    if(!Frame.activateVerbose){
+                        Frame.jTextArea2.setText("Error: No se encuentra la column: "+text+" En la table: "+tableCreate.name);
+                    }
+                    return "Error";
+                }
+                else{
+                	//ind = indice
+                    int ind = tableCreate.getColumnIndex(found.name);
+                    colIndices.add(ind);
+                    pkCols.add(found);
+
+                }
+            }
+            boolean hayNulos = tableCreate.hasNullValues(colIndices);
+            if(hayNulos){
+                Debug.add("Error: La constraint: <<"+nombre+">> no puede agregarse porque existen tuples nulas para la llave primaria.");
+                if(!Frame.activateVerbose){
+                    Frame.jTextArea2.setText("Error: La constraint: <<"+nombre+">> no puede agregarse porque existen tuples nulas para la llave primaria.");
+                }
+                return "Error";
+            }
+            // Verificamos que no existan values duplicates o nulos para las tuples actuales
+            boolean duplicates = tableCreate.revisarDuplicados(colIndices);
+            if(duplicates){
+                Debug.add("Error: La constraint: <<"+nombre+">> no puede agregarse porque existen tuples duplicadas para la llave primaria.");
+                if(!Frame.activateVerbose){
+                    Frame.jTextArea2.setText("Error: La constraint: <<"+nombre+">> no puede agregarse porque existen tuples duplicadas para la llave primaria.");
+                }
+                return "Error";
+            }
+            //Creamos constraint
+            Constraint c = new Constraint(nombre,Constraint.PK,pkCols,tableCreate.name);
+            //Verificamos que no exista una constraint del mismo type con el mismo name
+            boolean existeConstraint = getConstraint(c,this.availableCons);
+            if(existeConstraint){
+                Debug.add("Error: La constraint  "+c.name+" Ya fue declarada "+tableCreate.name);
+                if(!Frame.activateVerbose){
+                    Frame.jTextArea2.setText("Error: La constraint  "+c.name+" Ya fue declarada "+tableCreate.name);
+                }
+                    return "Error";
+            }
+            return c;
+		}
+        else if(ctx.FOREIGN()!=null){ // Si es foreign key
+            String nombre = ctx.fkNombre().getText();
+            //Revisando que existan los name de las columns en la table local
+            ArrayList<Column> localCols = new ArrayList<Column>();
+            ArrayList<Integer> localIndexes = new ArrayList<Integer>();
+            for(ParseTree n:ctx.localids()){
+                String text = n.getText();
+                Column found = getColumn(text,colsCreate);
+
+                if(found==null){
+                    Debug.add("Error: No se encuentra la column: "+text+" En la table: "+tableCreate.name);
+                    if(!Frame.activateVerbose){
+                        Frame.jTextArea2.setText("Error: No se encuentra la column: "+text+" En la table: "+tableCreate.name);
+                    }
+
+                    return "Error";
+                }
+                else{
+                    localCols.add(found);
+                    int in = tableCreate.getColumnIndex(found.nombre);
+                    localIndexes.add(in);
+                }
+            }
+            //Obteniendo la table que referencia
+            String refTable = ctx.idTabla().getText();
+
+            DBMetaData bd = DBMS.metaData.getDB(DBMS.currentDB);
+            TableMetaData t = bd.findTable(refTable);
+
+
+            if(t==null){
+                    Debug.add("Error: No se encuentra la table de referencia: "+refTable);
+                    if(!Frame.activateVerbose){
+                        Frame.jTextArea2.setText("Error: No se encuentra la table de referencia: "+refTable);
+                    }
+                    return "Error";
+            }
+
+
+            //Si encontramos la table procedemos a buscar las columns
+            else{
+                Table tablaRef = Table.loadTable(refTable);
+                ArrayList<Constraint> foreignConstraints = Table.loadConstraints(refTable);
+                ArrayList<Column> fkCols = new ArrayList<Column>();
+                ArrayList<Integer> fkIndexes = new ArrayList<Integer>();
+                ArrayList<Column> cols = Table.loadColums(refTable);
+                //Buscamos las columns de la primary key de la table foranea
+                ArrayList<Column> columnasPrimary = new ArrayList<Column>();
+                for(Constraint c1: foreignConstraints){
+                    if(c1.type == Constraint.PK){
+                        columnasPrimary.addAll(c1.colsPkeys);
+                    }
+
+                }
+                if(cols==null){
+                    Debug.add("Error: No se encuentra la table de referencia: "+refTable);
+                    if(!Frame.activateVerbose){
+                        Frame.jTextArea2.setText("Error: No se encuentra la table de referencia: "+refTable);
+                    }
+                    return "Error";
+                }
+
+                for(ParseTree n:ctx.refids()){
+                    String text = n.getText();
+                    //Buscamos las columns de la table
+
+                    if(cols==null){
+                        Debug.add("Error: No se encuentra archivo de columns para la table: "+refTable);
+                        if(!Frame.activateVerbose){
+                            Frame.jTextArea2.setText("Error: No se encuentra archivo de columns para la table: "+refTable);
+                        }
+                        return "Error";
+                    }
+                    Column found = getColumn(text,cols);
+
+                    if(found==null){
+                        Debug.add("Error: No se encuentra la column: "+text+" En la table: "+tableCreate.nombre);
+                        if(!Frame.activateVerbose){
+                            Frame.jTextArea2.setText("Error: No se encuentra la column: "+text+" En la table: "+tableCreate.nombre);
+                        }
+                        return "Error";
+                    }
+                    else{
+                        //Si encontramos la column, verificamos que la column pertenezca al primary key de la table externa para garantizar que la llave sea unica
+
+                        Column encontrada2 = getColumn(found.nombre,columnasPrimary);
+                        if(encontrada2==null){
+                            Debug.add("Error: No se puede crear la llave foranea. La column de referecia: "+found.nombre+" No es unica ");
+                            if(!Frame.activateVerbose){
+                                Frame.jTextArea2.setText("ERROR: No se puede crear la llave foranea. La column de referecia: "+found.nombre+" No es unica ");
+                            }
+                            return "Error";
+                        }
+                        //Agregamos la column
+                        fkCols.add(found);
+                        int in = tablaRef.getColumnIndex(found.nombre);
+                        fkIndexes.add(in);
+
+                    }
+                }
+
+                //Una vez obtenidos los dos arreglos de columns verificamos que tengan el mismo tamaño
+                if(fkCols.size()!=localCols.size()){
+                    Debug.add("Error: El numero de columns locales y remotas en la foregin key debe ser el mismo");
+                    if(!Frame.activateVerbose){
+                        Frame.jTextArea2.setText("Error: El numero de columns locales y remotas en la foregin key debe ser el mismo");
+                    }
+
+                    return "Error";
+                }
+                //Si los arreglos son iguales verificamos que tengan los mismos type
+                for(int i =0;i<localCols.size();i++){
+                    Column local = localCols.get(i);
+                    Column foreign = fkCols.get(i);
+                    if(local.type!=foreign.type){
+                        Debug.add("Error: las columns: '"+local.nombre+", "+foreign.nombre+"' Deben tener el mismo type");
+                        if(!Frame.activateVerbose){
+                            Frame.jTextArea2.setText("Error: las columns: '"+local.nombre+", "+foreign.nombre+"' Deben tener el mismo type");
+                        }
+                        return "Error";
+
+                    }
+                }
+                //Para cada tuple de la table local verificamos que los values existan en la table de referencia
+                for(Tuple tuple: tableCreate.tuples){
+                    //Obtenemos los values de la tuple actual
+                    ArrayList<Object> currValues = new ArrayList<Object>();
+                    for(int i : localIndexes){
+                        Object value = tuple.values.get(i);
+                        currValues.add(value);
+                    }
+                    //Revisamos si los values actuales existen en la table de referencia
+                    boolean existenValores = tablaRef.containsValue(currValues, fkIndexes);
+                    if(!existenValores){
+                        Debug.add("ERROR: no se puede crear la restriccion <<"+nombre+">> porque los values de las tuples no existen en la table de referencia.");
+                        if(!Frame.activateVerbose){
+                            Frame.jTextArea2.setText("ERROR: no se puede crear la restriccion <<"+nombre+">> porque los values de las tuples no existen en la table de referencia.");
+                        }
+                        return "ERROR";
+                    }
+
+                }
+                //Si todas las columns tienen los mismo tipos, procedemos a crear la constraint
+                Constraint c = new Constraint(nombre,Constraint.FK,localCols,fkCols,refTable,this.tableCreate.nombre);
+                //Verificamos que no exista una constraint del mismo type con el mismo nombre
+                boolean existeConstraint = getConstraint(c,this.availableCons);
+                if(existeConstraint){
+                        Debug.add("ERROR: La constraint  "+c.nombre+" Ya fue declarada "+tableCreate.nombre);
+                        if(!Frame.activateVerbose){
+                            Frame.jTextArea2.setText("ERROR: La constraint  "+c.nombre+" Ya fue declarada "+tableCreate.nombre);
+                        }
+
+                        return "ERROR";
+                }
+
+                return c;
+
+            }
+
+
+        }
+
+        else if(ctx.CHECK()!=null){
+            String nombre= ctx.chNombre().getText();
+            //Obtenemos la expresion del CHECK
+            Object e = visit(ctx.expression());
+            String expr = ctx.expression().getText();
+            //Verificamos que no existan errores en la expresion para poder castear
+            if(! (e instanceof Expression)){
+                return "Error";
+
+            }
+            Expression e1 = (Expression)e;
+            //Verificamos que las tuples actuales de la table cumplan con la restriccion
+            Table temp = new Table();
+            temp.nombre = tableCreate.nombre;
+            temp.columns.addAll(tableCreate.columns);
+            temp.tuples.addAll(tableCreate.tuples);
+            Loader.iterator = new TableIterator(temp,0);
+            for(int i =0; i<Loader.iterator.table.tuples.size();i++){
+                try {
+                if(!e1.isTrue()){
+                    Debug.add("Error: no se puede insertar constraint <<"+nombre+">> porque algunas tuples no cumplen con la restriccion.");
+                    if(!Frame.activateVerbose){
+                        Frame.jTextArea2.setText("Error: no se puede insertar constraint <<"+nombre+">> porque algunas tuples no cumplen con la restriccion.");
+                    }
+                    return "Error";
+                }
+                } catch (Exception ex) {
+                    Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            Constraint c = new Constraint(nombre,Constraint.CHECK,e1,tableCreate.nombre,expr);
+            //Verificamos que no exista una constraint del mismo type con el mismo name
+            boolean existeConstraint = getConstraint(c,this.availableCons);
+            if(existeConstraint){
+                    Debug.add("Error: La constraint  "+c.nombre+" Ya fue declarada "+tableCreate.nombre);
+                    if(!Frame.activateVerbose){
+                        Frame.jTextArea2.setText("Error: La constraint  "+c.nombre+" Ya fue declarada "+tableCreate.nombre);
+                    }
+                    return "Error";
+            }
+            return c;
+
+
+        }
+        return "Error";
+	}
+	
+	@Override
 	public Object visitVal(ValContext ctx) {
-		//Se debe agregar Column de la metadata para ver el tipo de valor
+		//Se debe agregar Column de la metadata para ver el tipo de valor se debe 
+		//reconocer que aqui pide la class columna.java, la cual debemos ingresar enuestra metadata
 		if(ctx.CHAR_VAL()!=null){
             return Column.CHAR_TYPE;
 
