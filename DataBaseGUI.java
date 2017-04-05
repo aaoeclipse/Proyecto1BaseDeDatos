3bf6f96e2c0c8783ebecd6d89d5fa7876f520af9
luisa2125 <luiyi2125@gmail.com>
2017-04-04 18:46:41 -0600

    GUI

---
+++ DataBaseGUI.java
@@ -0,0 +1,47 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+
+
+public class DataBaseGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DataBaseGUI window = new DataBaseGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public DataBaseGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 90, 3100, 1500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(640, 100, 2375, 431);
+		frame.getContentPane().add(textPane);
+	}
+}
